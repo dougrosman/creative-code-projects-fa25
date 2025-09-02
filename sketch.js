91119eb6030c92a8fe1e28653bc0bdb02795582b
Doug Rosman <dougrosman@gmail.com>
2025-09-02 13:33:56 -0500

    add assignment 2

--- sketch.js
+++ sketch.js
@@ -2,11 +2,24 @@ console.log("hello!")
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
+    noCursor()
 }
 
 function draw() {
-    // background(220)
-    ellipse(mouseX, mouseY, 40, 40)
+    clear()
+    ellipse(mouseX, mouseY, 30, 30)
+
+    const step = width/40;
+    for(let x = 0; x < width; x+=step) {
+        let s = map(x, 0, width, .01, 1)
+        if(x > width/2) {
+            s = map(x, 0, width, 1, 0.001)
+        }
+        
+        strokeWeight(s)
+        line(x, 0, mouseX, mouseY-10)
+        line(x, height, mouseX, mouseY+10)
+    }
 }
 
 function windowResized() {
